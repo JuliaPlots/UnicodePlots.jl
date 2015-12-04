70bf5ea2a7c2592d352643078ef27228c7f8f1a5
Christof Stocker <christof.stocker@aimc.at>
2015-12-04 23:22:38 +0100

    make stair complay to coding convention

--- src/interface/stairs.jl
+++ src/interface/stairs.jl
@@ -1,47 +1,50 @@
-function computeStairLines{F<:Real,R<:Real}(X::AbstractVector{F}, Y::AbstractVector{R}, style::Symbol)
-  if style == :post
-    xVec = zeros(length(X) * 2 - 1)
-    yVec = zeros(length(X) * 2 - 1)
-    xVec[1] = X[1]
-    yVec[1] = Y[1]
-    o = 0
-    for i = 2:(length(X))
-      xVec[i + o] = X[i]
-      xVec[i + o + 1] = X[i]
-      yVec[i + o] = Y[i-1]
-      yVec[i + o + 1] = Y[i]
-      o += 1
+function compute_stair_lines{F<:Real,R<:Real}(
+        X::AbstractVector{F},
+        Y::AbstractVector{R},
+        style::Symbol)
+    if style == :post
+        x_vex = zeros(length(X) * 2 - 1)
+        y_vex = zeros(length(X) * 2 - 1)
+        x_vex[1] = X[1]
+        y_vex[1] = Y[1]
+        o = 0
+        for i = 2:(length(X))
+            x_vex[i + o] = X[i]
+            x_vex[i + o + 1] = X[i]
+            y_vex[i + o] = Y[i-1]
+            y_vex[i + o + 1] = Y[i]
+            o += 1
+        end
+        x_vex, y_vex
+    elseif style == :pre
+        x_vex = zeros(length(X) * 2 - 1)
+        y_vex = zeros(length(X) * 2 - 1)
+        x_vex[1] = X[1]
+        y_vex[1] = Y[1]
+        o = 0
+        for i = 2:(length(X))
+            x_vex[i + o] = X[i-1]
+            x_vex[i + o + 1] = X[i]
+            y_vex[i + o] = Y[i]
+            y_vex[i + o + 1] = Y[i]
+            o += 1
+        end
+        x_vex, y_vex
     end
-    xVec, yVec
-  elseif style == :pre
-    xVec = zeros(length(X) * 2 - 1)
-    yVec = zeros(length(X) * 2 - 1)
-    xVec[1] = X[1]
-    yVec[1] = Y[1]
-    o = 0
-    for i = 2:(length(X))
-      xVec[i + o] = X[i-1]
-      xVec[i + o + 1] = X[i]
-      yVec[i + o] = Y[i]
-      yVec[i + o + 1] = Y[i]
-      o += 1
-    end
-    xVec, yVec
-  end
 end
 
 function stairs{F<:Real,R<:Real}(
-    X::Vector{F}, Y::Vector{R};
-    style::Symbol = :post,
-    args...)
-  xVec, yVec = computeStairLines(X, Y, style)
-  lineplot(xVec, yVec; args...)
+        X::Vector{F}, Y::Vector{R};
+        style::Symbol = :post,
+        args...)
+    x_vex, y_vex = compute_stair_lines(X, Y, style)
+    lineplot(x_vex, y_vex; args...)
 end
 
 function stairs!{T<:Canvas,F<:Real,R<:Real}(
-    plot::Plot{T}, X::AbstractVector{F}, Y::AbstractVector{R};
-    style::Symbol = :post,
-    args...)
-  xVec, yVec = computeStairLines(X, Y, style)
-  lineplot!(plot, xVec, yVec; args...)
+        plot::Plot{T}, X::AbstractVector{F}, Y::AbstractVector{R};
+        style::Symbol = :post,
+        args...)
+    x_vex, y_vex = compute_stair_lines(X, Y, style)
+    lineplot!(plot, x_vex, y_vex; args...)
 end
