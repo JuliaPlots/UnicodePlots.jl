059b5ff9d1723e5b2b9088886a0378c7fe06c0fc
Christof Stocker <christof.stocker@aimc.at>
2015-12-06 13:28:57 +0100

    make rounding helper functions comply to coding conventions

--- src/common.jl
+++ src/common.jl
@@ -1,39 +1,39 @@
-ceilNegLog10{F<:AbstractFloat}(x::F) = ceil(Integer, -log10(x))
-roundNegLog10{F<:AbstractFloat}(x::F) = round(Integer, -log10(x), RoundNearestTiesUp)
-roundUpToTick{F<:AbstractFloat,R<:Real}(x::F,m::R) = x == 0. ? 0.: (x > 0 ? ceil(x, ceilNegLog10(m)) : -floor(-x, ceilNegLog10(m)))
-roundDownToTick{F<:AbstractFloat,R<:Real}(x::F,m::R) = x == 0. ? 0.: (x > 0 ? floor(x, ceilNegLog10(m)) : -ceil(-x, ceilNegLog10(m)))
-roundUpToSubTick{F<:AbstractFloat,R<:Real}(x::F,m::R) = x == 0. ? 0.: (x > 0 ? ceil(x, ceilNegLog10(m)+1) : -floor(-x, ceilNegLog10(m)+1))
-roundDownToSubTick{F<:AbstractFloat,R<:Real}(x::F,m::R) = x == 0. ? 0.: (x > 0 ? floor(x, ceilNegLog10(m)+1) : -ceil(-x, ceilNegLog10(m)+1))
-floatRoundLog10{F<:AbstractFloat,R<:Real}(x::F,m::R) = x == 0. ? 0.: (x > 0 ? round(x, ceilNegLog10(m)+1) : -round(-x, ceilNegLog10(m)+1))
-floatRoundLog10{F<:AbstractFloat}(x::F) = x > 0 ? floatRoundLog10(x,x): floatRoundLog10(x,-x)
+ceil_neg_log10{F<:AbstractFloat}(x::F) = ceil(Integer, -log10(x))
+round_neg_log10{F<:AbstractFloat}(x::F) = round(Integer, -log10(x), RoundNearestTiesUp)
+round_up_tick{F<:AbstractFloat,R<:Real}(x::F,m::R) = x == 0. ? 0.: (x > 0 ? ceil(x, ceil_neg_log10(m)) : -floor(-x, ceil_neg_log10(m)))
+round_down_tick{F<:AbstractFloat,R<:Real}(x::F,m::R) = x == 0. ? 0.: (x > 0 ? floor(x, ceil_neg_log10(m)) : -ceil(-x, ceil_neg_log10(m)))
+round_up_subtick{F<:AbstractFloat,R<:Real}(x::F,m::R) = x == 0. ? 0.: (x > 0 ? ceil(x, ceil_neg_log10(m)+1) : -floor(-x, ceil_neg_log10(m)+1))
+round_down_subtick{F<:AbstractFloat,R<:Real}(x::F,m::R) = x == 0. ? 0.: (x > 0 ? floor(x, ceil_neg_log10(m)+1) : -ceil(-x, ceil_neg_log10(m)+1))
+float_round_log10{F<:AbstractFloat,R<:Real}(x::F,m::R) = x == 0. ? 0.: (x > 0 ? round(x, ceil_neg_log10(m)+1)::Float64 : -round(-x, ceil_neg_log10(m)+1)::Float64)
+float_round_log10{F<:AbstractFloat}(x::F) = x > 0 ? float_round_log10(x,x) : float_round_log10(x,-x)
 
-function plottingRange{F<:AbstractFloat,R<:AbstractFloat}(xmin::F, xmax::R)
-  diffX = xmax - xmin
-  xmax = roundUpToTick(xmax, diffX)
-  xmin = roundDownToTick(xmin, diffX)
-  xmin, xmax
+function plotting_range{F<:AbstractFloat,R<:AbstractFloat}(xmin::F, xmax::R)
+    diffX = xmax - xmin
+    xmax = round_up_tick(xmax, diffX)
+    xmin = round_down_tick(xmin, diffX)
+    xmin, xmax
 end
 
-function plottingRangeNarrow{F<:AbstractFloat,R<:AbstractFloat}(xmin::F, xmax::R)
-  diffX = xmax - xmin
-  xmax = roundUpToSubTick(xmax, diffX)
-  xmin = roundDownToSubTick(xmin, diffX)
-  xmin, xmax
+function plotting_range_narrow{F<:AbstractFloat,R<:AbstractFloat}(xmin::F, xmax::R)
+    diffX = xmax - xmin
+    xmax = round_up_subtick(xmax, diffX)
+    xmin = round_down_subtick(xmin, diffX)
+    xmin, xmax
 end
 
 function extend_limits(vec, limits)
-  mi = float(minimum(limits))
-  ma = float(maximum(limits))
-  if mi == 0. && ma == 0.
-    mi = minimum(vec)
-    ma = maximum(vec)
-  end
-  diff = ma - mi
-  if diff == 0
-    ma = mi + 1
-    mi = mi - 1
-  end
-  (limits == [0.,0.]) ? plottingRangeNarrow(mi, ma) : plottingRangeNarrow(mi, ma)
+    mi = float(minimum(limits))
+    ma = float(maximum(limits))
+    if mi == 0. && ma == 0.
+        mi = minimum(vec)
+        ma = maximum(vec)
+    end
+    diff = ma - mi
+    if diff == 0
+        ma = mi + 1
+        mi = mi - 1
+    end
+    (limits == [0.,0.]) ? plotting_range_narrow(mi, ma) : plotting_range_narrow(mi, ma)
 end
 
 const borderMap = Dict{Symbol,Dict{Symbol,UTF8String}}()
@@ -109,15 +109,15 @@ colorEncode[:yellow]=0b100
 colorEncode[:green]=0b101
 colorEncode[:cyan]=0b110
 for k in keys(colorEncode)
-  v = colorEncode[k]
-  colorDecode[v]=k
+    v = colorEncode[k]
+    colorDecode[v]=k
 end
 colorDecode[0b111]=:white
 
 function printColor(color::UInt8, io::IO, args...)
-  col = colorDecode[color]
-  str = string(args...)
-  print_with_color(col, io, str)
+    col = colorDecode[color]
+    str = string(args...)
+    print_with_color(col, io, str)
 end
 
 # ▁▂▃▄▅▆▇█
--- src/interface/histogram.jl
+++ src/interface/histogram.jl
@@ -1,17 +1,17 @@
 # code by IainNZ
 function histogram(x, bins::Int; symb = "▇", args...)
-  edges, counts = hist(x, bins)
-  labels = Array(UTF8String, length(counts))
-  binwidth = edges.step / edges.divisor
-  @inbounds for i in 1:length(counts)
-		val = floatRoundLog10(edges[i])
-    labels[i] = string("(", val, ",", floatRoundLog10(val+binwidth), "]")
-  end
-  barplot(labels, counts; symb = symb, args...)
+    edges, counts = hist(x, bins)
+    labels = Array(UTF8String, length(counts))
+    binwidth = edges.step / edges.divisor
+    @inbounds for i in 1:length(counts)
+        val = float_round_log10(edges[i])
+        labels[i] = string("(", val, ",", float_round_log10(val+binwidth), "]")
+    end
+    barplot(labels, counts; symb = symb, args...)
 end
 
 function histogram(x; bins::Int = sturges(length(x)), args...)
-  histogram(x, bins; args...)
+    histogram(x, bins; args...)
 end
 
 # Sturges' rule
