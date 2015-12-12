f27aed15ef3b3a698bf6e046eab09a33ef01ca94
Christof Stocker <christof.stocker@aimc.at>
2015-12-12 22:10:53 +0100

    make densityplot comply with coding standard

--- src/interface/densityplot.jl
+++ src/interface/densityplot.jl
@@ -1,20 +1,21 @@
 function densityplot{F<:Real,R<:Real}(
-    x::Vector{F}, y::Vector{R};
-    color::Symbol = :white,
-    args...)
-  X = convert(Vector{Float64}, x)
-  Y = convert(Vector{Float64}, y)
-  minX = minimum(X); minY = minimum(Y)
-  maxX = maximum(X); maxY = maximum(Y)
-  newPlot = Plot(X, Y, DensityCanvas; grid = false, args...)
-  points!(newPlot, X, Y, color)
+        x::Vector{F},
+        y::Vector{R};
+        color::Symbol = :white,
+        args...)
+    X = convert(Vector{Float64}, x)
+    Y = convert(Vector{Float64}, y)
+    new_plot = Plot(X, Y, DensityCanvas; grid = false, args...)
+    points!(new_plot, X, Y, color)
 end
 
 function densityplot!{T<:Canvas,F<:Real,R<:Real}(
-    plot::Plot{T}, x::Vector{F}, y::Vector{R};
-    color::Symbol = :white,
-    args...)
-  X = convert(Vector{Float64}, x)
-  Y = convert(Vector{Float64}, y)
-  points!(plot, X, Y, color)
+        plot::Plot{T},
+        x::Vector{F},
+        y::Vector{R};
+        color::Symbol = :white,
+        args...)
+    X = convert(Vector{Float64}, x)
+    Y = convert(Vector{Float64}, y)
+    points!(plot, X, Y, color)
 end
