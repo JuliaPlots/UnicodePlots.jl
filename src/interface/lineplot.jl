1cb72858b3d8c61b572cb920a1b211e97075107c
Christof Stocker <christof.stocker@aimc.at>
2015-12-14 10:14:56 +0100

    @milktrader - document lineplot

--- src/interface/lineplot.jl
+++ src/interface/lineplot.jl
@@ -1,3 +1,102 @@
+"""
+`lineplot(x, y; nargs...)` → `Plot`
+
+Description
+============
+
+Draws a path through the given points on a new canvas.
+It uses the first parameter `x`
+(which should be a vector or range) to denote
+the horizontal position of each point,
+and the second parameter `y`
+(which should also be a vector or range)
+as their vertical position.
+This means that the two vectors have to have the same length.
+
+Usage
+======
+
+    lineplot(x, y; title = "", name = "", width = 40, height = 15, border = :solid, xlim = [0, 0], ylim = [0, 0], margin = 3, padding = 1, color = :auto, labels = true, canvas = BrailleCanvas, grid = true)
+
+    lineplot(fun[, start, end]; title = "", name = "", width = 40, height = 15, border = :solid, xlim = [0, 0], ylim = [0, 0], margin = 3, padding = 1, color = :auto, labels = true, canvas = BrailleCanvas, grid = true)
+
+Arguments
+==========
+
+- **`x`** : The horizontal dimension for each point. Can be a Real number or of type `TimeType`
+
+- **`y`** : The vertical dimension for each point.
+
+- **`fun`** : A function f: R -> R that should be drawn from `start` to `end`
+
+- **`title`** : Text to display on the top of the plot.
+
+- **`name`** : Annotation of the current drawing to displayed on the right
+
+- **`width`** : Number of characters per row that should be used for plotting.
+
+- **`height`** : Number of rows that should be used for plotting. Not applicable to `barplot`.
+
+- **`border`** : The style of the bounding box of the plot. Supports `:solid`, `:bold`, `:dashed`, `:dotted`, `:ascii`, and `:none`.
+
+- **`xlim`** : Plotting range for the x coordinate. `[0, 0]` stands for automatic.
+
+- **`ylim`** : Plotting range for the y coordinate. `[0, 0]` stands for automatic.
+
+- **`margin`** : Number of empty characters to the left of the whole plot.
+
+- **`padding`** : Space of the left and right of the plot between the labels and the canvas.
+
+- **`color`** : Color of the drawing. Can be any of `:blue`, `:red`, `:yellow`
+
+- **`labels`** : Can be used to hide the labels by setting `labels=false`.
+
+- **`canvas`** : The type of canvas that should be used for drawing.
+
+- **`grid`** : Can be used to hide the gridlines at the origin
+
+Returns
+========
+
+A plot object of type `Plot{T<:Canvas}`
+
+Author(s)
+==========
+
+- Christof Stocker (Github: https://github.com/Evizero)
+- Milktrader (Github: https://github.com/milktrader)
+
+Examples
+=========
+
+    julia> lineplot([1, 2, 7], [9, -6, 8], title = "My Lineplot")
+
+                        My Lineplot
+         ┌────────────────────────────────────────┐
+       9 │⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+         │⢱⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠊│
+         │⠈⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⠁⠀⠀│
+         │⠀⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠊⠁⠀⠀⠀⠀│
+         │⠀⠘⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⠊⠀⠀⠀⠀⠀⠀⠀│
+         │⠀⠀⢇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠔⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+         │⠀⠀⠸⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+         │⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+         │⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠒⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+         │⠉⠉⠉⠉⡏⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⢉⠭⠋⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁│
+         │⠀⠀⠀⠀⢱⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+         │⠀⠀⠀⠀⠘⡄⠀⠀⠀⠀⠀⠀⠀⠀⡠⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+         │⠀⠀⠀⠀⠀⢣⠀⠀⠀⠀⠀⢀⠔⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+         │⠀⠀⠀⠀⠀⠸⡀⠀⠀⡠⠔⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+      -6 │⠀⠀⠀⠀⠀⠀⢇⡠⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+         └────────────────────────────────────────┘
+         1                                        7
+
+
+See also
+=========
+
+`Plot`, `scatter`, `stairs`, `BrailleCanvas`, `BlockCanvas`, `AsciiCanvas`
+"""
 function lineplot{F<:Real, R<:Real}(
         X::AbstractVector{F},
         Y::AbstractVector{R};
