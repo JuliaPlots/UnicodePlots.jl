2dee03490b1a0bfedeb9a9a73cc00b5e7f9ba310
Christof Stocker <c.stocker@hci-kdd.org>
2015-12-24 16:06:20 +0100

    add basic doc for DotCanvas

--- src/graphics/dotcanvas.jl
+++ src/graphics/dotcanvas.jl
@@ -6,6 +6,19 @@ dot_decode[0b01 + 1] = '.'
 dot_decode[0b10 + 1] = '\''
 dot_decode[0b11 + 1] = ':'
 
+"""
+Similar to the `AsciiCanvas`, the `DotCanvas` only uses
+ASCII characters to draw it's content. Naturally,
+it doesn't look quite as nice as the Unicode-based
+ones. However, in some situations it might yield
+better results. Printing plots to a file is one
+of those situations.
+
+The DotCanvas is best utilized in combination
+with `scatterplot`.
+For `lineplot` we suggest to use the `AsciiCanvas`
+instead.
+"""
 type DotCanvas <: LookupCanvas
     grid::Array{UInt8,2}
     colors::Array{UInt8,2}
