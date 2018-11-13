aa81d2b5132dc38371393cee621abe456f9621d9
Christof Stocker <stocker.christof@gmail.com>
2018-11-13 18:32:50 +0100

    refactor histogram

--- src/interface/histogram.jl
+++ src/interface/histogram.jl
@@ -1,43 +1,60 @@
 """
-`histogram(v; nargs...)` → `Plot`
+    histogram(data; kwargs...)
 
 Description
 ============
 
-Draws a horizontal histogram of the given vector `v`.
-If the parameter `bins` is not specified,
-then it will be chosen based on *sturges rule*.
-Note that `histogram` is a simply wrapper for `barplot`,
-which means that it supports the same parameter arguments.
+Draws a horizontal histogram of the given `data`. The positional
+parameter `data` can either be a `StatsBase.Histogram`, or some
+`AbstractArray`.  In the later case the `Histogram` will be
+fitted automatically.
+
+Note internally that `histogram` is a simply wrapper for
+`barplot`, which means that it supports the same keyword
+arguments.
 
 Usage
 ======
 
-    histogram(v; bins = sturges(length(v)), symb = "▇", border = :solid, title = "", margin = 3, padding = 1, color = :green, width = 40, labels = true)
+    histogram(x; nbins, closed = :left, kwargs...)
+
+    histogram(hist; symb = "▇", border = :solid, title = "", margin = 3, padding = 1, color = :green, width = 40, labels = true)
 
 Arguments
 ==========
 
-- **`v`** : Vector or range for which the histogram should be computed.
+- **`x`** : Array of numbers for which the histogram should be computed.
 
-- **`bins`** : The approximate number of bins that should be used.
+- **`nbins`** : The approximate number of bins that should be used.
+
+- **`closed`** : If `:left` (the default), the bin intervals are
+  left-closed ``[a,b)``; if `:right`, intervals are right-closed
+  ``(a,b]``.
 
 - **`border`** : The style of the bounding box of the plot.
-Supports `:solid`, `:bold`, `:dashed`, `:dotted`, `:ascii`, and `:none`.
+  Supports `:corners`, `:solid`, `:bold`, `:dashed`, `:dotted`,
+  `:ascii`, and `:none`.
 
 - **`title`** : Text to display on the top of the plot.
 
-- **`margin`** : Number of empty characters to the left of the whole plot.
+- **`margin`** : Number of empty characters to the left of the
+  whole plot.
 
-- **`padding`** : Space of the left and right of the plot between the labels and the canvas.
+- **`padding`** : Space of the left and right of the plot between
+  the labels and the canvas.
 
-- **`color`** : Color of the drawing. Can be any of `:blue`, `:red`, `:yellow`
+- **`color`** : Color of the drawing.
+  Can be any of `:green`, `:blue`, `:red`, `:yellow`, `:cyan`,
+  `:magenta`, `:white`, `:normal`
 
-- **`width`** : Number of characters per row that should be used for plotting.
+- **`width`** : Number of characters per row that should be used
+  for plotting.
 
-- **`labels`** : Can be used to hide the labels by setting `labels=false`.
+- **`labels`** : Boolean. Can be used to hide the labels by
+  setting `labels = false`.
 
-- **`symb`** : Specifies the character that should be used to render the bars
+- **`symb`** : Specifies the character that should be used to
+  render the individual bars.
 
 Returns
 ========
@@ -54,35 +71,33 @@ Author(s)
 Examples
 =========
 
-    julia> histogram(randn(1000), bins = 15, title = "Histogram")
-
-                                 Histogram
-                  ┌────────────────────────────────────────┐
-      (-3.5,-3.0] │ 1                                      │
-      (-3.0,-2.5] │ 2                                      │
-      (-2.5,-2.0] │▇▇▇▇ 22                                 │
-      (-2.0,-1.5] │▇▇▇▇▇▇▇▇▇▇ 53                           │
-      (-1.5,-1.0] │▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 78                       │
-      (-1.0,-0.5] │▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 141          │
-       (-0.5,0.0] │▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 191 │
-        (0.0,0.5] │▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 193 │
-        (0.5,1.0] │▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 150         │
-        (1.0,1.5] │▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 101                  │
-        (1.5,2.0] │▇▇▇▇▇▇▇▇ 42                             │
-        (2.0,2.5] │▇▇▇ 17                                  │
-        (2.5,3.0] │▇ 4                                     │
-        (3.0,3.5] │▇ 5                                     │
-                  └────────────────────────────────────────┘
-                                 Frequency
+```julia-repl
+julia> histogram(randn(1000) * 0.1, closed = :right, nbins = 15)
+                  ┌                                        ┐
+   (-0.3 , -0.25] ┤▇ 4
+   (-0.25, -0.2 ] ┤▇▇ 12
+   (-0.2 , -0.15] ┤▇▇▇▇▇▇▇▇▇ 48
+   (-0.15, -0.1 ] ┤▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 114
+   (-0.1 , -0.05] ┤▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 143
+   (-0.05, -0.0 ] ┤▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 177
+   ( 0.0 ,  0.05] ┤▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 192
+   ( 0.05,  0.1 ] ┤▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 152
+   ( 0.1 ,  0.15] ┤▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ 83
+   ( 0.15,  0.2 ] ┤▇▇▇▇▇▇▇▇ 45
+   ( 0.2 ,  0.25] ┤▇▇▇▇ 23
+   ( 0.25,  0.3 ] ┤▇ 6
+   ( 0.3 ,  0.35] ┤ 1
+                  └                                        ┘
+                                  Frequency
+```
 
 See also
 =========
 
-`hist`, `Plot`, `barplot`, `BarplotGraphics`
+[`Plot`](@ref), [`barplot`](@ref), [`BarplotGraphics`](@ref)
 """
-function histogram(v, bins::Int; symb = "▇", args...)
-    result = fit(Histogram, v; nbins = bins, closed=:right)
-    edges, counts = result.edges[1], result.weights
+function histogram(hist::Histogram; symb = "▇", kw...)
+    edges, counts = hist.edges[1], hist.weights
     labels = Vector{String}(undef, length(counts))
     binwidth = typeof(edges.step) == Float64 ? edges.step : edges.step.hi
     # compute label padding based on all labels.
@@ -97,13 +112,15 @@ function histogram(v, bins::Int; symb = "▇", args...)
         pad_right = max(pad_right, a1[2], a2[2])
     end
     # compute the labels using the computed padding
+    l_str = hist.closed == :right ? "(" : "["
+    r_str = hist.closed == :right ? "]" : ")"
     for i in 1:length(counts)
         val1 = float_round_log10(edges[i], binwidth)
         val2 = float_round_log10(val1+binwidth, binwidth)
         a1 = Base.alignment(IOBuffer(), val1)
         a2 = Base.alignment(IOBuffer(), val2)
         labels[i] =
-            "\e[90m(\e[0m" *
+            "\e[90m" * l_str * "\e[0m" *
             repeat(" ", pad_left - a1[1]) *
             string(val1) *
             repeat(" ", pad_right - a1[2]) *
@@ -111,16 +128,23 @@ function histogram(v, bins::Int; symb = "▇", args...)
             repeat(" ", pad_left - a2[1]) *
             string(val2) *
             repeat(" ", pad_right - a2[2]) *
-            "\e[90m]\e[0m"
+            "\e[90m" * r_str * "\e[0m"
     end
-    plt = barplot(labels, counts; symb = symb, args...)
+    plt = barplot(labels, counts; symb = symb, kw...)
     xlabel!(plt, "Frequency")
     plt
 end
 
-function histogram(v; bins::Int = sturges(length(v)), args...)
-    histogram(v, bins; args...)
+function histogram(x; bins = nothing, closed = :left, kw...)
+    if bins != nothing
+        Base.depwarn("The keyword parameter `bins` is deprecated, use `nbins` instead", :histogram)
+        hist = fit(Histogram, x; nbins = bins, closed = closed)
+    else
+        hargs = filter(p -> p.first == :nbins, kw)
+        hist = fit(Histogram, x; closed = closed, hargs...)
+    end
+    args = filter(p -> p.first != :nbins, kw)
+    histogram(hist; args...)
 end
 
-# Sturges' rule
-sturges(n) = n == 0 ? 1 : ceil(Int, log2(n) + 1)
+@deprecate histogram(x::AbstractArray, nbins::Int; kw...) histogram(x; nbins=nbins, kw...)
