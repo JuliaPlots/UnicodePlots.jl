ebf3847414d43bfc141a8e3f86aa672cb501f7a6
t-bltg <tf.bltg@gmail.com>
2021-09-13 15:48:45 +0200

    Add unit test

--- test/tst_plot.jl
+++ test/tst_plot.jl
@@ -1,10 +1,12 @@
-canvas = BrailleCanvas(40, 10, origin_x = 0., origin_y = 0., width = 1., height = 1.)
-p = @inferred Plot(canvas)
-@test_throws ArgumentError Plot(canvas, margin=-1)
-test_ref("references/plot/empty.txt", @show_col(p))
-canvas = BrailleCanvas(30, 5, origin_x = 0., origin_y = 0., width = 1., height = 1.)
-p = @inferred Plot(canvas)
-test_ref("references/plot/empty_small.txt", @show_col(p))
+@testset "empty" begin
+    canvas = BrailleCanvas(40, 10, origin_x = 0., origin_y = 0., width = 1., height = 1.)
+    p = @inferred Plot(canvas)
+    @test_throws ArgumentError Plot(canvas, margin=-1)
+    test_ref("references/plot/empty.txt", @show_col(p))
+    canvas = BrailleCanvas(30, 5, origin_x = 0., origin_y = 0., width = 1., height = 1.)
+    p = @inferred Plot(canvas)
+    test_ref("references/plot/empty_small.txt", @show_col(p))
+end
 
 canvas = BrailleCanvas(40, 10, origin_x = 0., origin_y = 0., width = 1., height = 1.)
 lines!(canvas, 0., 0., 1., 1., :blue)
@@ -12,71 +14,87 @@ lines!(canvas, .2, .7, 1., 0., :red)
 lines!(canvas, 0., 2., .5, 0., :green)
 points!(canvas, .5, .9)
 
-p = @inferred Plot(canvas)
-test_ref("references/plot/canvas_only.txt", @print_col(p))
-test_ref("references/plot/canvas_only.txt", @show_col(p))
+@testset "canvas / xylabel" begin
+    p = @inferred Plot(canvas)
+    test_ref("references/plot/canvas_only.txt", @print_col(p))
+    test_ref("references/plot/canvas_only.txt", @show_col(p))
 
-for border in (:solid, :corners, :barplot, :bold, :ascii, :none, :dashed, :dotted)
-    local p = @inferred Plot(canvas, border = border)
-    test_ref("references/plot/border_$(string(border)).txt", @show_col(p))
+    for border in (:solid, :corners, :barplot, :bold, :ascii, :none, :dashed, :dotted)
+        local p = @inferred Plot(canvas, border = border)
+        test_ref("references/plot/border_$(string(border)).txt", @show_col(p))
+    end
+
+    p = @inferred Plot(canvas, xlabel = "x", ylabel= "y")
+    test_ref("references/plot/xylabel.txt", @show_col(p))
 end
 
-p = @inferred Plot(canvas, xlabel = "x", ylabel= "y")
-test_ref("references/plot/xylabel.txt", @show_col(p))
+@testset "padding" begin
+    p = @inferred Plot(canvas, title = "testtitle", xlabel = "x", ylabel= "y", margin = 4, padding = 5)
+    @test @inferred(label!(p, :l, ":l auto 1")) === p
+    @test @inferred(label!(p, :r, ":r auto 1", :red)) === p
+    test_ref("references/plot/padding.txt", @show_col(p))
 
-p = @inferred Plot(canvas, title = "testtitle", xlabel = "x", ylabel= "y", margin = 4, padding = 5)
-@test @inferred(label!(p, :l, ":l auto 1")) === p
-@test @inferred(label!(p, :r, ":r auto 1", :red)) === p
-test_ref("references/plot/padding.txt", @show_col(p))
-
-p = @inferred Plot(canvas, title = "testtitle", xlabel = "x", ylabel= "y", margin = 4, padding = 5, labels = false)
-@test @inferred(label!(p, :l, ":l auto 1")) === p
-@test @inferred(label!(p, :r, ":r auto 1", :red)) === p
-test_ref("references/plot/padding_nolabels.txt", @show_col(p))
-
-p = @inferred Plot(canvas, title = "testtitle")
-test_ref("references/plot/title.txt", @show_col(p))
-@test @inferred(label!(p, :l, ":l auto 1")) === p
-@test @inferred(label!(p, :r, ":r auto 1", :red)) === p
-@test @inferred(label!(p, :l, ":l auto 2", :green)) === p
-@test @inferred(label!(p, :r, ":r auto 2", :blue)) === p
-test_ref("references/plot/title_auto.txt", @show_col(p))
-
-for i in 1:10
-    @test @inferred(label!(p, :l, i, "$i", color=:green)) === p
-    @test @inferred(label!(p, :r, i, "$i", :yellow)) === p
+    p = @inferred Plot(canvas, title = "testtitle", xlabel = "x", ylabel= "y", margin = 4, padding = 5, labels = false)
+    @test @inferred(label!(p, :l, ":l auto 1")) === p
+    @test @inferred(label!(p, :r, ":r auto 1", :red)) === p
+    test_ref("references/plot/padding_nolabels.txt", @show_col(p))
 end
-@test_throws ArgumentError label!(p, :bl, 5, ":l  5", :red)
-@test_throws ArgumentError label!(p, :d, ":l auto", :red)
-@test @inferred(label!(p, :l, 5, ":l  5", :red)) === p
-@test @inferred(label!(p, :r, 5, "5  :r", color=:blue)) === p
-@test @inferred(label!(p, :t, ":t", color=:yellow)) === p
-@test @inferred(label!(p, :tl, ":tl", :cyan)) === p
-@test @inferred(label!(p, :tr, ":tr")) === p
-label!(p, :bl, ":bl", :blue)
-label!(p, :b, ":b", :green)
-label!(p, :br, ":br", :white)
 
-@test @inferred(lines!(p, 0., 1., 1., 0., color=(0,0,255))) === p
-@test @inferred(pixel!(p, 10, 1, color = (255,255,0))) === p
-@test @inferred(points!(p, 0.05, .75, color = (0,255,0))) === p
+@testset "title / labels / full deco" begin
+    p = @inferred Plot(canvas, title = "testtitle")
+    test_ref("references/plot/title.txt", @show_col(p))
+    @test @inferred(label!(p, :l, ":l auto 1")) === p
+    @test @inferred(label!(p, :r, ":r auto 1", :red)) === p
+    @test @inferred(label!(p, :l, ":l auto 2", :green)) === p
+    @test @inferred(label!(p, :r, ":r auto 2", :blue)) === p
+    test_ref("references/plot/title_auto.txt", @show_col(p))
 
-ttl = "title!(plot, text)"
-@test @inferred(title!(p, ttl)) === p
-@test @inferred(title(p)) == ttl
-xlab = "xlabel!(plot, text)"
-@test @inferred(xlabel!(p, xlab)) === p
-@test @inferred(xlabel(p)) == xlab
-ylab = "ylabel!(plot, text)"
-@test @inferred(ylabel!(p, ylab)) === p
-@test @inferred(ylabel(p)) == ylab
+    for i in 1:10
+        @test @inferred(label!(p, :l, i, "$i", color=:green)) === p
+        @test @inferred(label!(p, :r, i, "$i", :yellow)) === p
+    end
+    @test_throws ArgumentError label!(p, :bl, 5, ":l  5", :red)
+    @test_throws ArgumentError label!(p, :d, ":l auto", :red)
+    @test @inferred(label!(p, :l, 5, ":l  5", :red)) === p
+    @test @inferred(label!(p, :r, 5, "5  :r", color=:blue)) === p
+    @test @inferred(label!(p, :t, ":t", color=:yellow)) === p
+    @test @inferred(label!(p, :tl, ":tl", :cyan)) === p
+    @test @inferred(label!(p, :tr, ":tr")) === p
+    label!(p, :bl, ":bl", :blue)
+    label!(p, :b, ":b", :green)
+    label!(p, :br, ":br", :white)
 
-test_ref("references/plot/full_deco.txt", @show_col(p))
-tmp = tempname()
-savefig(p, tmp; color=true)
-test_ref("references/plot/full_deco.txt", read(tmp, String))
+    @test @inferred(lines!(p, 0., 1., 1., 0., color=(0,0,255))) === p
+    @test @inferred(pixel!(p, 10, 1, color = (255,255,0))) === p
+    @test @inferred(points!(p, 0.05, .75, color = (0,255,0))) === p
 
-test_ref("references/plot/full_deco_nocolor.txt", @show_nocol(p))
-tmp = tempname()
-savefig(p, tmp; color=false)
-test_ref("references/plot/full_deco_nocolor.txt", read(tmp, String))
+    ttl = "title!(plot, text)"
+    @test @inferred(title!(p, ttl)) === p
+    @test @inferred(title(p)) == ttl
+    xlab = "xlabel!(plot, text)"
+    @test @inferred(xlabel!(p, xlab)) === p
+    @test @inferred(xlabel(p)) == xlab
+    ylab = "ylabel!(plot, text)"
+    @test @inferred(ylabel!(p, ylab)) === p
+    @test @inferred(ylabel(p)) == ylab
+
+    test_ref("references/plot/full_deco.txt", @show_col(p))
+    tmp = tempname()
+    savefig(p, tmp; color=true)
+    test_ref("references/plot/full_deco.txt", read(tmp, String))
+
+    test_ref("references/plot/full_deco_nocolor.txt", @show_nocol(p))
+    tmp = tempname()
+    savefig(p, tmp; color=false)
+    test_ref("references/plot/full_deco_nocolor.txt", read(tmp, String))
+end
+
+@testset "annotations" begin
+    p = @inferred Plot(canvas)
+    @test @inferred(annotate!(p, 0.5, 0.5, "Center")) === p
+    @test @inferred(annotate!(p, 0., 0., "Bottom Left"; halign=:left)) === p
+    @test @inferred(annotate!(p, 1., 0., "Bottom Right"; halign=:right)) === p
+    @test @inferred(annotate!(p, 0., 1., "Top Left"; halign=:left)) === p
+    @test @inferred(annotate!(p, 1., 1., "Top Right"; halign=:right)) === p
+    test_ref("references/plot/annotations.txt", @show_col(p))
+end
