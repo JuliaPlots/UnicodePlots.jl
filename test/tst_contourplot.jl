44d5b918dae1fb853f9b0b058c6b0406782ee8b7
t-bltg <tf.bltg@gmail.com>
2022-01-17 15:33:23 +0100

    add contourplot test and ref files

---
+++ test/tst_contourplot.jl
@@ -0,0 +1,23 @@
+gaussian_2d(x = -3:.01:3, y = -7:.01:3; x₀=0, y₀=-2, σx=1, σy=2) = begin
+    X = repeat(x', length(y), 1)
+    Y = repeat(y, 1, length(x))
+    x, y, map((x, y) -> exp(-((x - x₀) / 2σx)^2 - ((y - y₀) / 2σy)^2), X, Y)
+end
+
+@testset "arbitrary colormap" begin
+    colormap = :cividis
+    p = @inferred contourplot(gaussian_2d()...; colormap = colormap)
+    test_ref("references/contourplot/gauss_$colormap.txt", @show_col(p))
+end
+
+@testset "number of levels" begin
+    levels = 3
+    p = @inferred contourplot(gaussian_2d()...; levels = levels)
+    test_ref("references/contourplot/gauss_$(levels)levels.txt", @show_col(p))
+end
+
+@testset "update contourplot" begin
+    p = @inferred contourplot(gaussian_2d()...; levels = 2)
+    contourplot!(p, gaussian_2d(; σx = .5, σy = .25)...; levels = 1, colormap = :magma)
+    test_ref("references/contourplot/gauss_nested.txt", @show_col(p))
+end
