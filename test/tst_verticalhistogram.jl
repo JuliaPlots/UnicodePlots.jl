573aafd18c44ce61e26e43e4113707a9f37f4b6d
t-bltg <tf.bltg@gmail.com>
2022-02-07 08:57:56 +0100

    format

--- test/tst_verticalhistogram.jl
+++ test/tst_verticalhistogram.jl
@@ -1,7 +1,6 @@
 seed!(RNG, 1337)
 x = randn(RNG, 10000)
 
-
 @testset "basic" begin
     p = @inferred verticalhistogram(x)
     # test_ref("references/verticalhistogram/basic.txt", @print_col(p))  # TODO plot should be used, I need the team help to apply their style. 
