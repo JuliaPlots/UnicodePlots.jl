8ceee88cafb131e78fbee42cc89880e3d2eb93c8
t-bltg <tf.bltg@gmail.com>
2022-09-07 15:41:17 +0200

    add precompilation statements

--- src/UnicodePlots.jl
+++ src/UnicodePlots.jl
@@ -2,6 +2,7 @@ module UnicodePlots
 
 using SparseArrays: AbstractSparseMatrix, findnz
 using StatsBase: Histogram, fit, percentile
+using SnoopPrecompile
 using LinearAlgebra
 using ColorSchemes
 using StaticArrays
@@ -112,5 +113,34 @@ include("interface/heatmap.jl")
 include("interface/spy.jl")
 include("interface/boxplot.jl")
 include("interface/polarplot.jl")
+
+@precompile_setup begin
+    buf = IOContext(PipeBuffer(), :color => true, :displaysize => displaysize(stdout))
+    @precompile_all_calls begin
+        plots = (
+            lineplot(1:2),
+            lineplot(1:2, 1:2),
+            lineplot(1:2, [0:1 2:3]),
+            lineplot([cos, sin], -π / 2, 2π),
+            scatterplot(1:2),
+            scatterplot(1:2, 1:2),
+            scatterplot(1:2, [0:1 2:3]),
+            stairs([1.0, 2, 4], [1, 3, 4]),
+            barplot(["Paris", "New York"], [2.244, 8.406]),
+            boxplot([1.0, 2, 3]),
+            boxplot(["one", "two"], [collect(1:3), collect(3:4)]),
+            histogram([1.0, 2, 3]),
+            histogram([1.0, 2, 3]; vertical = true),
+            polarplot(0:(π / 4):π, 0:4),
+            densityplot([1.0, 2, 3], [1.0, 2, 3]; dscale = x -> log(1 + x)),
+            heatmap(repeat(collect(0:4)', outer = (5, 1))),
+            spy([1 -1 0; -1 2 1; 0 -1 1]),
+            contourplot(-2:2, -2:2, (x, y) -> exp(-(x / 2)^2 - (y + 2)^2)),
+            surfaceplot(-1.0:1.0, -1.0:1.0, (x, y) -> sinc(√(x^2 + y^2)); lines = true),
+            isosurface(-1.0:1.0, -1.0:1.0, -1.0:1.0, (x, y, z) -> x^2 + y^2 - z^2 - 1),
+        )
+        foreach(p -> show(buf, p), plots)
+    end
+end
 
 end
