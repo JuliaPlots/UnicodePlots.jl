ceafecd60859dacd65a4e3c808e8f219d160abef
t-bltg <tf.bltg@gmail.com>
2026-09-20 13:12:47 +0200

    increase tol

--- UnicodePlots/test/tst_io.jl
+++ UnicodePlots/test/tst_io.jl
@@ -58,7 +58,7 @@ end
 
 macro measure(ex, tol, versioned)
     return quote
-        base_tol = is_ci() ? 5 : 1.25
+        base_tol = is_ci() ? 5.5 : 1.25
         @test string($ex; color = true) isa String  # 1st pass - ttfp
         if (
                 UnicodePlots.get_have_truecolor() &&
